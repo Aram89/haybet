9336b15faa5b01940a6c7468bac0ff9b154931c7
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-08 00:04:13 +0400

    change game object parametrs

--- haybet/src/main/java/org/proffart/bet/domain/Game.java
+++ haybet/src/main/java/org/proffart/bet/domain/Game.java
@@ -16,6 +16,11 @@ public class Game {
 	@Column(name="tournament_id")
 	private int tournamentID;
 	
+	@Column(name="hash")
+	private String hash;
+	@Column(name="u_string")
+	private String uString;
+	
 	@Column(name="name_en")
 	private String nameEN;
 	
@@ -28,89 +33,117 @@ public class Game {
 	@Column(name="date")
 	private String date;
 	
-	@Column(name="p1")
-	private double p1;
 	
-	@Column(name="p2")
-	private double p2;
+	@Column(name="t_p1")
+	private double t_p1;
+	@Column(name="t_p2")
+	private double t_p2;
+	@Column(name="t_x")
+	private double t_x;
+	@Column(name="t_1x")
+	private double t_1x;
+	@Column(name="t_12")
+	private double t_12;
+	@Column(name="t_x2")
+	private double t_x2;
 	
-	@Column(name="x")
-	private double x;
+	@Column(name="t_b_05")
+	private double t_b_05;
+	@Column(name="t_b_15")
+	private double t_b_15;
+	@Column(name="t_b_25")
+	private double t_b_25;
+	@Column(name="t_b_35")
+	private double t_b_35;
+	@Column(name="t_b_45")
+	private double t_b_45;
+	@Column(name="t_b_55")
+	private double t_b_55;
 	
-	@Column(name="x1")
-	private double x1;
+	@Column(name="t_m_05")
+	private double t_m_05;
+	@Column(name="t_m_15")
+	private double t_m_15;
+	@Column(name="t_m_25")
+	private double t_m_25;
+	@Column(name="t_m_35")
+	private double t_m_35;
+	@Column(name="t_m_45")
+	private double t_m_45;
+	@Column(name="t_m_55")
+	private double t_m_55;
 	
-	@Column(name="x2")
-	private double x2;
+	@Column(name="t_b1_05")
+	private double t_b1_05;
+	@Column(name="t_b1_15")
+	private double t_b1_15;
+	@Column(name="t_b1_25")
+	private double t_b1_25;
+	@Column(name="t_b1_35")
+	private double t_b1_35;
+	@Column(name="t_b1_45")
+	private double t_b1_45;
+	@Column(name="t_b1_55")
+	private double t_b1_55;
 	
-	@Column(name="x12")
-	private double x12;
+	@Column(name="t_m1_05")
+	private double t_m1_05;
+	@Column(name="t_m1_15")
+	private double t_m1_15;
+	@Column(name="t_m1_25")
+	private double t_m1_25;
+	@Column(name="t_m1_35")
+	private double t_m1_35;
+	@Column(name="t_m1_45")
+	private double t_m1_45;
+	@Column(name="t_m1_55")
+	private double t_m1_55;
 	
-	@Column(name="f1")
-	private double f1;
+	@Column(name="t_b2_05")
+	private double t_b2_05;
+	@Column(name="t_b2_15")
+	private double t_b2_15;
+	@Column(name="t_b2_25")
+	private double t_b2_25;
+	@Column(name="t_b2_35")
+	private double t_b2_35;
+	@Column(name="t_b2_45")
+	private double t_b2_45;
+	@Column(name="t_b2_55")
+	private double t_b2_55;
 	
-	@Column(name="f1_s")
-	private double f1_s;
+	@Column(name="t_m2_05")
+	private double t_m2_05;
+	@Column(name="t_m2_15")
+	private double t_m2_15;
+	@Column(name="t_m2_25")
+	private double t_m2_25;
+	@Column(name="t_m2_35")
+	private double t_m2_35;
+	@Column(name="t_m2_45")
+	private double t_m2_45;
+	@Column(name="t_m2_55")
+	private double t_m2_55;
 	
-	@Column(name="f2")
-	private double f2;
-	
-	@Column(name="f2_s")
-	private double f2_s;
-	
-	@Column(name="b")
-	private double b;
-	
-	@Column(name="b_S")
-	private double b_s;
-	
-	@Column(name="m")
-	private double m;
-	
-	@Column(name="m_S")
-	private double m_s;
-	
-	@Column(name="b1")
-	private double b1;
-	
-	@Column(name="b1_s")
-	private double b1_s;
-	
-	@Column(name="b2")
-	private double b2;
-	
-	@Column(name="b2_s")
-	private double b2_s;
-	
-	@Column(name="m1")
-	private double m1;
-	
-	@Column(name="m1_s")
-	private double m1_s;
-	
-	@Column(name="m2")
-	private double m2;
-	
-	@Column(name="m2_s")
-	private double m2_s;
+	@Column(name="t_t1_s_yes")
+	private double t_t1_s_yes;
+	@Column(name="t_t1_s_no")
+	private double t_t1_s_no;
+	@Column(name="t_t2_s_yes")
+	private double t_t2_s_yes;
+	@Column(name="t_t2_s_no")
+	private double t_t2_s_no;
+	@Column(name="t_s_yes")
+	private double t_s_yes;
+	@Column(name="t_s_no")
+	private double t_s_no;
 	
 	@Column(name="_x_totobet_id")
 	private int totoID;
 	
 	@Column(name="_x_tournament_totobet_id")
-	private int tournamentTotoID;	
-	
-	@Column(name="hash")
-	private String hash;
-	
-	public String getHash() {
-		return hash;
-	}
+	private int tournamentTotoID;
 
-	public void setHash(String hash) {
-		this.hash = hash;
-	}
-	
 	public int getId() {
 		return id;
 	}
@@ -125,6 +158,22 @@ public class Game {
 
 	public void setTournamentID(int tournamentID) {
 		this.tournamentID = tournamentID;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public String getuString() {
+		return uString;
+	}
+
+	public void setuString(String uString) {
+		this.uString = uString;
 	}
 
 	public String getNameEN() {
@@ -159,180 +208,388 @@ public class Game {
 		this.date = date;
 	}
 
-	public double getP1() {
-		return p1;
+	public double getT_p1() {
+		return t_p1;
 	}
 
-	public void setP1(double p1) {
-		this.p1 = p1;
+	public void setT_p1(double t_p1) {
+		this.t_p1 = t_p1;
 	}
 
-	public double getP2() {
-		return p2;
+	public double getT_p2() {
+		return t_p2;
 	}
 
-	public void setP2(double p2) {
-		this.p2 = p2;
+	public void setT_p2(double t_p2) {
+		this.t_p2 = t_p2;
 	}
 
-	public double getX() {
-		return x;
+	public double getT_x() {
+		return t_x;
 	}
 
-	public void setX(double x) {
-		this.x = x;
+	public void setT_x(double t_x) {
+		this.t_x = t_x;
 	}
 
-	public double getX1() {
-		return x1;
+	public double getT_1x() {
+		return t_1x;
 	}
 
-	public void setX1(double x1) {
-		this.x1 = x1;
+	public void setT_1x(double t_1x) {
+		this.t_1x = t_1x;
 	}
 
-	public double getX2() {
-		return x2;
+	public double getT_12() {
+		return t_12;
 	}
 
-	public void setX2(double x2) {
-		this.x2 = x2;
+	public void setT_12(double t_12) {
+		this.t_12 = t_12;
 	}
 
-	public double getX12() {
-		return x12;
+	public double getT_x2() {
+		return t_x2;
 	}
 
-	public void setX12(double x12) {
-		this.x12 = x12;
+	public void setT_x2(double t_x2) {
+		this.t_x2 = t_x2;
 	}
 
-	public double getF1() {
-		return f1;
+	public double getT_b_05() {
+		return t_b_05;
 	}
 
-	public void setF1(double f1) {
-		this.f1 = f1;
+	public void setT_b_05(double t_b_05) {
+		this.t_b_05 = t_b_05;
 	}
 
-	public double getF1_s() {
-		return f1_s;
+	public double getT_b_15() {
+		return t_b_15;
 	}
 
-	public void setF1_s(double f1_s) {
-		this.f1_s = f1_s;
+	public void setT_b_15(double t_b_15) {
+		this.t_b_15 = t_b_15;
 	}
 
-	public double getF2() {
-		return f2;
+	public double getT_b_25() {
+		return t_b_25;
 	}
 
-	public void setF2(double f2) {
-		this.f2 = f2;
+	public void setT_b_25(double t_b_25) {
+		this.t_b_25 = t_b_25;
 	}
 
-	public double getF2_s() {
-		return f2_s;
+	public double getT_b_35() {
+		return t_b_35;
 	}
 
-	public void setF2_s(double f2_s) {
-		this.f2_s = f2_s;
+	public void setT_b_35(double t_b_35) {
+		this.t_b_35 = t_b_35;
 	}
 
-	public double getB() {
-		return b;
+	public double getT_b_45() {
+		return t_b_45;
 	}
 
-	public void setB(double b) {
-		this.b = b;
+	public void setT_b_45(double t_b_45) {
+		this.t_b_45 = t_b_45;
 	}
 
-	public double getB_s() {
-		return b_s;
+	public double getT_b_55() {
+		return t_b_55;
 	}
 
-	public void setB_s(double b_s) {
-		this.b_s = b_s;
+	public void setT_b_55(double t_b_55) {
+		this.t_b_55 = t_b_55;
 	}
 
-	public double getM() {
-		return m;
+	public double getT_m_05() {
+		return t_m_05;
 	}
 
-	public void setM(double m) {
-		this.m = m;
+	public void setT_m_05(double t_m_05) {
+		this.t_m_05 = t_m_05;
 	}
 
-	public double getM_s() {
-		return m_s;
+	public double getT_m_15() {
+		return t_m_15;
 	}
 
-	public void setM_s(double m_s) {
-		this.m_s = m_s;
+	public void setT_m_15(double t_m_15) {
+		this.t_m_15 = t_m_15;
 	}
 
-	public double getB1() {
-		return b1;
+	public double getT_m_25() {
+		return t_m_25;
 	}
 
-	public void setB1(double b1) {
-		this.b1 = b1;
+	public void setT_m_25(double t_m_25) {
+		this.t_m_25 = t_m_25;
 	}
 
-	public double getB1_s() {
-		return b1_s;
+	public double getT_m_35() {
+		return t_m_35;
 	}
 
-	public void setB1_S(double b1_s) {
-		this.b1_s = b1_s;
-	}
-	
-	public double getB2() {
-		return b2;
+	public void setT_m_35(double t_m_35) {
+		this.t_m_35 = t_m_35;
 	}
 
-	public void setB2(double b2) {
-		this.b2 = b2;
+	public double getT_m_45() {
+		return t_m_45;
 	}
 
-	public double getB2_s() {
-		return b2_s;
+	public void setT_m_45(double t_m_45) {
+		this.t_m_45 = t_m_45;
 	}
 
-	public void setB2_s(double b2_s) {
-		this.b2_s = b2_s;
+	public double getT_m_55() {
+		return t_m_55;
 	}
 
-	public double getM1() {
-		return m1;
+	public void setT_m_55(double t_m_55) {
+		this.t_m_55 = t_m_55;
 	}
 
-	public void setM1(double m1) {
-		this.m1 = m1;
+	public double getT_b1_05() {
+		return t_b1_05;
 	}
 
-	public double getM1_s() {
-		return m1_s;
+	public void setT_b1_05(double t_b1_05) {
+		this.t_b1_05 = t_b1_05;
 	}
 
-	public void setM1_s(double m1_s) {
-		this.m1_s = m1_s;
+	public double getT_b1_15() {
+		return t_b1_15;
 	}
 
-	public double getM2() {
-		return m2;
+	public void setT_b1_15(double t_b1_15) {
+		this.t_b1_15 = t_b1_15;
 	}
 
-	public void setM2(double m2) {
-		this.m2 = m2;
+	public double getT_b1_25() {
+		return t_b1_25;
 	}
 
-	public double getM2_s() {
-		return m2_s;
+	public void setT_b1_25(double t_b1_25) {
+		this.t_b1_25 = t_b1_25;
 	}
 
-	public void setM2_s(double m2_s) {
-		this.m2_s = m2_s;
+	public double getT_b1_35() {
+		return t_b1_35;
+	}
+
+	public void setT_b1_35(double t_b1_35) {
+		this.t_b1_35 = t_b1_35;
+	}
+
+	public double getT_b1_45() {
+		return t_b1_45;
+	}
+
+	public void setT_b1_45(double t_b1_45) {
+		this.t_b1_45 = t_b1_45;
+	}
+
+	public double getT_b1_55() {
+		return t_b1_55;
+	}
+
+	public void setT_b1_55(double t_b1_55) {
+		this.t_b1_55 = t_b1_55;
+	}
+
+	public double getT_m1_05() {
+		return t_m1_05;
+	}
+
+	public void setT_m1_05(double t_m1_05) {
+		this.t_m1_05 = t_m1_05;
+	}
+
+	public double getT_m1_15() {
+		return t_m1_15;
+	}
+
+	public void setT_m1_15(double t_m1_15) {
+		this.t_m1_15 = t_m1_15;
+	}
+
+	public double getT_m1_25() {
+		return t_m1_25;
+	}
+
+	public void setT_m1_25(double t_m1_25) {
+		this.t_m1_25 = t_m1_25;
+	}
+
+	public double getT_m1_35() {
+		return t_m1_35;
+	}
+
+	public void setT_m1_35(double t_m1_35) {
+		this.t_m1_35 = t_m1_35;
+	}
+
+	public double getT_m1_45() {
+		return t_m1_45;
+	}
+
+	public void setT_m1_45(double t_m1_45) {
+		this.t_m1_45 = t_m1_45;
+	}
+
+	public double getT_m1_55() {
+		return t_m1_55;
+	}
+
+	public void setT_m1_55(double t_m1_55) {
+		this.t_m1_55 = t_m1_55;
+	}
+
+	public double getT_b2_05() {
+		return t_b2_05;
+	}
+
+	public void setT_b2_05(double t_b2_05) {
+		this.t_b2_05 = t_b2_05;
+	}
+
+	public double getT_b2_15() {
+		return t_b2_15;
+	}
+
+	public void setT_b2_15(double t_b2_15) {
+		this.t_b2_15 = t_b2_15;
+	}
+
+	public double getT_b2_25() {
+		return t_b2_25;
+	}
+
+	public void setT_b2_25(double t_b2_25) {
+		this.t_b2_25 = t_b2_25;
+	}
+
+	public double getT_b2_35() {
+		return t_b2_35;
+	}
+
+	public void setT_b2_35(double t_b2_35) {
+		this.t_b2_35 = t_b2_35;
+	}
+
+	public double getT_b2_45() {
+		return t_b2_45;
+	}
+
+	public void setT_b2_45(double t_b2_45) {
+		this.t_b2_45 = t_b2_45;
+	}
+
+	public double getT_b2_55() {
+		return t_b2_55;
+	}
+
+	public void setT_b2_55(double t_b2_55) {
+		this.t_b2_55 = t_b2_55;
+	}
+
+	public double getT_m2_05() {
+		return t_m2_05;
+	}
+
+	public void setT_m2_05(double t_m2_05) {
+		this.t_m2_05 = t_m2_05;
+	}
+
+	public double getT_m2_15() {
+		return t_m2_15;
+	}
+
+	public void setT_m2_15(double t_m2_15) {
+		this.t_m2_15 = t_m2_15;
+	}
+
+	public double getT_m2_25() {
+		return t_m2_25;
+	}
+
+	public void setT_m2_25(double t_m2_25) {
+		this.t_m2_25 = t_m2_25;
+	}
+
+	public double getT_m2_35() {
+		return t_m2_35;
+	}
+
+	public void setT_m2_35(double t_m2_35) {
+		this.t_m2_35 = t_m2_35;
+	}
+
+	public double getT_m2_45() {
+		return t_m2_45;
+	}
+
+	public void setT_m2_45(double t_m2_45) {
+		this.t_m2_45 = t_m2_45;
+	}
+
+	public double getT_m2_55() {
+		return t_m2_55;
+	}
+
+	public void setT_m2_55(double t_m2_55) {
+		this.t_m2_55 = t_m2_55;
+	}
+
+	public double getT_t1_s_yes() {
+		return t_t1_s_yes;
+	}
+
+	public void setT_t1_s_yes(double t_t1_s_yes) {
+		this.t_t1_s_yes = t_t1_s_yes;
+	}
+
+	public double getT_t1_s_no() {
+		return t_t1_s_no;
+	}
+
+	public void setT_t1_s_no(double t_t1_s_no) {
+		this.t_t1_s_no = t_t1_s_no;
+	}
+
+	public double getT_t2_s_yes() {
+		return t_t2_s_yes;
+	}
+
+	public void setT_t2_s_yes(double t_t2_s_yes) {
+		this.t_t2_s_yes = t_t2_s_yes;
+	}
+
+	public double getT_t2_s_no() {
+		return t_t2_s_no;
+	}
+
+	public void setT_t2_s_no(double t_t2_s_no) {
+		this.t_t2_s_no = t_t2_s_no;
+	}
+
+	public double getT_s_yes() {
+		return t_s_yes;
+	}
+
+	public void setT_s_yes(double t_s_yes) {
+		this.t_s_yes = t_s_yes;
+	}
+
+	public double getT_s_no() {
+		return t_s_no;
+	}
+
+	public void setT_s_no(double t_s_no) {
+		this.t_s_no = t_s_no;
 	}
 
 	public int getTotoID() {
@@ -369,4 +626,6 @@ public class Game {
 			return true;
 		return false;		
 	}
+
+
 }
