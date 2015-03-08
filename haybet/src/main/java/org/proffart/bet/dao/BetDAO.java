19a2945813f606d41b12dcb4bd7b4f4c13f3b13c
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-09 00:38:26 +0400

    crateGroup and crateBet

--- haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
@@ -3,6 +3,7 @@ package org.proffart.bet.dao;
 public interface BetDAO {
 	
 	public void setStatus();
+	public int crateGroup(double amount, double coefficient, int betsCount);
+	public int crateBet(int groupID, int gameID, String betType);
 	
-
 }
--- haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
@@ -1,10 +1,38 @@
 package org.proffart.bet.dao;
 
+import org.proffart.bet.domain.Bet;
+import org.proffart.bet.domain.BetGroup;
+
 public class BetDAOImpl extends AbstractDAO implements BetDAO {
 
 	public void setStatus() {
 		
 		
 	}
+	
+	public int crateGroup(double amount, double coefficient, int betsCount) {
+		int groupID = 0;
+		BetGroup betGroup = new BetGroup();
+		//betGroup.setUserID(userID); TODO user get session
+		betGroup.setAmount(amount);
+		betGroup.setCoefficient(coefficient);
+		betGroup.setBetsCount(betsCount);
+		getSession().save(betGroup);
+		groupID = betGroup.getID();
+		return groupID;
+	}
+	
+	public int crateBet(int groupID, int gameID, String betType) {
+		int betID = 0;
+		Bet bet = new Bet();
+		bet.setBetGroupID(groupID);
+		bet.setGameID(gameID);
+		bet.setBetType(betType);
+		getSession().save(bet);
+		betID = bet.getID();
+		return betID; 
+	}
+	
+	
 
 }
