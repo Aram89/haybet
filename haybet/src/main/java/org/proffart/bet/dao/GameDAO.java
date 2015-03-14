bc71417f35ae02e6731780ab54b3c2f701d41d37
Aram89 <aramkirakosyan89@gmail.com>
2015-03-14 02:25:08 -0700

    Update GameDAO.java

--- haybet/src/main/java/org/proffart/bet/dao/GameDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/GameDAO.java
@@ -7,8 +7,9 @@ import org.proffart.bet.domain.Game;
 public interface GameDAO {
 	public void addGame(Game game);
 	public void addGames(List<Game> games);
-	public Game getGameByID(int gameID);
 	public List<Game> getGames4Tournament(int tournamentID);
 	public List<Game> getGames(Integer minID, Integer maxID);
+	public int getGameIdByHash(String hash);
+	public List<Game> getGamesByDate();
 
 }
