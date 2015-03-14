29290eeb74332934f1b77dd780afc765fda53e30
Aram89 <aramkirakosyan89@gmail.com>
2015-03-14 02:23:58 -0700

    Update ResultDAOImpl.java

--- haybet/src/main/java/org/proffart/bet/dao/ResultDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/ResultDAOImpl.java
@@ -2,17 +2,70 @@ package org.proffart.bet.dao;
 
 import java.util.List;
 
-import org.proffart.bet.domain.Result;
+import org.hibernate.Query;
+import org.proffart.bet.domain.Game;
+import org.springframework.stereotype.Repository;
 
-public class ResultDAOImpl extends AbstractDAO implements ResultDAO {
+@Repository
+public class GameDAOImpl extends AbstractDAO implements GameDAO {
 
-	public void saveResult(Result result) {
-		persist(result);		
+	public void addGame(Game game) {
+		//persist(game);
+		getSession().saveOrUpdate(game);
+		
+		
 	}
 
-	public void saveResults(List<Result> results) {
-		for(Result result : results){
-			saveResult(result);
+	public void addGames(List<Game> games) {
+		for(Game game : games){
+			addGame(game);
 		}		
 	}
+
+	public List<Game> getGames4Tournament(int tournamentID) {
+		String hql = "FROM org.proffart.bet.domain.Game g where g.tournamentTotoID = :id ";
+		Query query = getSession().createQuery(hql);
+		query.setParameter("id",tournamentID);
+		@SuppressWarnings("unchecked")
+		List<Game> results = query.list();
+		return results;		
+	}
+
+	public List<Game> getGames(Integer minID, Integer maxID) {
+		String hql = "FROM org.proffart.bet.domain.Game g where g.id > :minID AND g.id < :maxID";
+		Query query = getSession().createQuery(hql);
+		query.setParameter("minID",minID);
+		query.setParameter("maxID",maxID);
+		@SuppressWarnings("unchecked")
+		List<Game> results = query.list();
+		return results;		
+	}
+
+	@Override
+	public int getGameIdByHash(String hash) {
+		Integer gameId = null;
+		try{
+			String hql = "SELECT id FROM org.proffart.bet.domain.User where hash= :hash ";
+			Query query = getSession().createQuery(hql);
+			query.setParameter("hash",hash);
+			@SuppressWarnings("unchecked")
+			List results = query.list();
+			gameId = (Integer) results.get(0);					
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return gameId;		
+	}
+
+	@Override
+	public List<Game> getGamesByDate() {
+			//String hql = "FROM org.proffart.bet.domain.Game g where g.date <= now()";
+			String hql = "select new map(g.id, g.hash) from Game g where g.date <= now()";
+			Query query = getSession().createQuery(hql);
+			@SuppressWarnings("unchecked")
+			List<Game> results = query.list();
+			return results;		
+		}
+		
 }
