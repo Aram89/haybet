e98b880e1559f341e7a8e65e90994f1a5f1e9737
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-14 11:50:54 +0400

    status set wait

--- haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
@@ -15,10 +15,12 @@ public class BetDAOImpl extends AbstractDAO implements BetDAO {
 	public int crateGroup(double amount, double coefficient, int betsCount) {
 		int groupID = 0;
 		BetGroup betGroup = new BetGroup();
-		//betGroup.setUserID(userID); TODO user get session
+		betGroup.setUserID(0); //TODO user get session
 		betGroup.setAmount(amount);
 		betGroup.setCoefficient(coefficient);
 		betGroup.setBetsCount(betsCount);
+		betGroup.setFinishedBetsCount(0);
+		betGroup.setStatus(WAIT);
 		getSession().save(betGroup);
 		groupID = betGroup.getID();
 		return groupID;
@@ -30,6 +32,8 @@ public class BetDAOImpl extends AbstractDAO implements BetDAO {
 		bet.setBetGroupID(groupID);
 		bet.setGameID(gameID);
 		bet.setBetType(betType);
+		bet.setIsFinished(false);
+		bet.setStatus(WAIT);
 		getSession().save(bet);
 		betID = bet.getID();
 		return betID; 
