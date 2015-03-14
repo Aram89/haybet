3d7c12459f90d6c7c49cf3e88efd92c3caec36b5
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-14 11:49:47 +0400

    add status define

--- haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
@@ -1,6 +1,9 @@
 package org.proffart.bet.dao;
 
 public interface BetDAO {
+	final public static String OK = "OK";
+	final public static String NOK = "NOK";
+	final public static String WAIT = "WAIT";
 	
 	public void setStatus();
 	public int crateGroup(double amount, double coefficient, int betsCount);
