4c756a7a7d778496e939822c3ca1eb1f17aa61a3
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 21:03:16 +0400

    Result

--- haybet/src/main/java/org/proffart/bet/domain/Result.java
+++ haybet/src/main/java/org/proffart/bet/domain/Result.java
@@ -1,26 +1,42 @@
 package org.proffart.bet.domain;
 
-public class Result {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "result")
+public class Result {
+	@Id
+	@Column(name = "id")
 	private int gameID;
+	@Column(name = "score")
+	private String score;
+	@Column(name = "date")
+	private String date;
+
 	public int getGameID() {
 		return gameID;
 	}
+
 	public void setGameID(int gameID) {
 		this.gameID = gameID;
 	}
-	public String getuString() {
-		return uString;
+
+	public String getDate() {
+		return date;
 	}
-	public void setuString(String uString) {
-		this.uString = uString;
+
+	public void setDate(String date) {
+		this.date = date;
 	}
+
 	public String getScore() {
 		return score;
 	}
+
 	public void setScore(String score) {
 		this.score = score;
 	}
-	private String uString;
-	private String score;
 }
