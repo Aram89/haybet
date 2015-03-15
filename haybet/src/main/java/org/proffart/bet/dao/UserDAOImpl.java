8f9c7c530b842e18b398f4432a239484510b1ca3
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 14:56:10 +0400

    SuppressWarnings("rawtypes")

--- haybet/src/main/java/org/proffart/bet/dao/UserDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/UserDAOImpl.java
@@ -3,39 +3,32 @@ package org.proffart.bet.dao;
 import java.util.List;
 
 import org.hibernate.Query;
-import org.hibernate.SessionFactory;
 import org.proffart.bet.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository 
-public class UserDAOImpl extends AbstractDAO implements UserDAO{
-	
-	@Autowired
-	private SessionFactory sessionFactory;
-
+public class UserDAOImpl extends AbstractDAO implements UserDAO {
 	
 	public void addUser(User user) {
 		getSession().save(user);
-		//persist(user);		
 	}
 
 	public void deleteUser(Integer id) {
-				
+		
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<User> getUsers() {
-		return sessionFactory.getCurrentSession().createQuery("FROM users").list();
+		return (List<User>) getSession().createQuery("FROM users").list();
 	}
 
 	public String getPassword(String userName) {
 		String hql = "SELECT password FROM org.proffart.bet.domain.User where nickName='gyada'";
-		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		Query query = getSession().createQuery(hql);
+		@SuppressWarnings("rawtypes")
 		List results = query.list();
 		String pass = results.get(0).toString();
-		System.out.println(pass);
 		return pass;
-		//return null;
 	}
 
 }
