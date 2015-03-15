58ba20ba564e4e0fcd47380676fc4f7eeb061a1e
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 14:59:13 +0400

    change type to java

---
+++ haybet/src/main/java/org/proffart/bet/controller/ResultController.java
@@ -0,0 +1,37 @@
+package org.proffart.bet.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+import org.proffart.bet.domain.TmpResult;
+import org.proffart.bet.service.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ResultController {
+	
+	@Autowired
+	ResultService service;
+	
+	@RequestMapping(value="results", method = RequestMethod.GET)
+	public void showResults(ModelMap model, HttpSession session){
+		try{
+			String countries = FileReader.readJson("C:\\Users\\Aram\\Downloads\\results.json");
+			ObjectMapper mapper = new ObjectMapper();
+			List<TmpResult> results = mapper.readValue(countries, TypeFactory.defaultInstance().constructCollectionType(List.class,  
+					   TmpResult.class));
+			service.saveResults(results);			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}		
+	}
+
+}
