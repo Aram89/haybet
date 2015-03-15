119af39b34369695175fb755953f2bdbaee98988
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 14:56:36 +0400

    add define

--- haybet/src/main/java/org/proffart/bet/dao/UserDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/UserDAO.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.proffart.bet.domain.User;
 
 public interface UserDAO {
+	final public static String GUEST = "GUEST";
+	final public static String USER  = "USER";
+	final public static String ADMIN = "ADMIN";
+	
 	public void addUser(User user);
 	public String getPassword(String userName);
 	public void deleteUser(Integer id);
