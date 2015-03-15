5c6f80f9da60d778d6362042822f5d81312ddc4c
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 15:06:38 +0400

    getBets

--- haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
@@ -1,12 +1,17 @@
 package org.proffart.bet.dao;
 
+import java.util.List;
+
+import org.proffart.bet.domain.Bet;
+
 public interface BetDAO {
 	final public static String OK = "OK";
 	final public static String NOK = "NOK";
 	final public static String WAIT = "WAIT";
 	
 	public void setStatus();
-	public int createGroup(double amount, double coefficient, int betsCount);
-	public int createBet(int groupID, int gameID, String betType);
+	public Integer createGroup(Double amount, Double coefficient, Integer betsCount);
+	public Integer createBet(Integer groupID, Integer gameID, String betType);
+	public List <Bet> getBets();
 	
 }
