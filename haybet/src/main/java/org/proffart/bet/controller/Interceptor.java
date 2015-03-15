45f5c8e2f97f3ff98a14c92b7137ddfb8b564d9f
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-15 14:53:18 +0400

    Revert "Revert "Interceptor ""
    This reverts commit d5a1eeeb901622bb5090167e9ee93b5114bd2688.

--- haybet/src/main/java/org/proffart/bet/controller/Interceptor.java
+++ haybet/src/main/java/org/proffart/bet/controller/Interceptor.java
@@ -4,53 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
+import org.proffart.bet.dao.UserDAO;
+import org.proffart.bet.domain.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class Interceptor implements HandlerInterceptor{
 
-	public Boolean isFirsTime = true;
-	public void afterCompletion(HttpServletRequest req,
-			HttpServletResponse resp, Object handler, Exception e)
-			throws Exception {
-		System.out.println("after request");		
+	public void afterCompletion(HttpServletRequest req, HttpServletResponse resp, Object handler, Exception e) throws Exception {
+		//System.out.println("after request");
 	}
 
-	public void postHandle(HttpServletRequest req, HttpServletResponse resp,
-			Object handler, ModelAndView model) throws Exception {
-		System.out.println("post handle");
-		
+	public void postHandle(HttpServletRequest req, HttpServletResponse resp, Object handler, ModelAndView model) throws Exception {
+		//System.out.println("post handle");
 	}
 
-	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp,
-			Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 		HttpSession session = req.getSession();
+		boolean isLogined = false;
 		
-		System.out.println("pre handle");
-		//System.out.println("COUNTRIES ARE:" + FileReader.readJson("C:\\Users\\Aram\\Downloads\\country.json"));
+		User user = (User) session.getAttribute("userobj");
 		
-		String isLogined = (String) session.getAttribute("userobj");
-		
-		if (isLogined == null || isLogined.equals("guest") && req.isRequestedSessionIdValid()){
-			System.out.println(isFirsTime);
-			isFirsTime = false;
-			System.out.println(isLogined);
-			System.out.println(req.getContextPath());
-			//resp.sendRedirect("/register");
-			resp.sendRedirect(req.getContextPath() + "/login");
-			//req.getRequestDispatcher("login").forward(req, resp);
-			System.out.println(req);
-			req.getAttribute("uri");
-			//req.isRequestedSessionIdValid()
-			return true;
-		}
-		
-		System.out.println(req.isRequestedSessionIdValid());
-		System.out.println(req.getSession().getId());
-		System.out.println(session.getId());
-		System.out.println(isLogined);		
-		return true;//isLogined;
+		if(req.isRequestedSessionIdValid() && user instanceof User) {
+			isLogined = user.getRole() != UserDAO.GUEST;
+		}		
+		return isLogined;
 	}
 }
