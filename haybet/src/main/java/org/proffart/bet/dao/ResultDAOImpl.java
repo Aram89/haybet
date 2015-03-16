4b1ce79cc9fe48bd059b7572016d966acbe37638
Aram89 <aramkirakosyan89@gmail.com>
2015-03-15 23:38:51 -0700

    Added @Repository

--- haybet/src/main/java/org/proffart/bet/dao/ResultDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/ResultDAOImpl.java
@@ -3,7 +3,9 @@ package org.proffart.bet.dao;
 import java.util.List;
 
 import org.proffart.bet.domain.Result;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ResultDAOImpl extends AbstractDAO implements ResultDAO {
 
 	public void saveResult(Result result) {
