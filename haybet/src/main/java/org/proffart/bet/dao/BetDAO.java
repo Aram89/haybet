40e55703b80b36919ed7a0b44f152acd16b180d4
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-18 22:00:45 +0400

    createGroup add userID argumnet

--- haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAO.java
@@ -1,6 +1,5 @@
 package org.proffart.bet.dao;
 
-import java.util.List;
 import java.util.Map;
 
 import org.proffart.bet.domain.Bet;
@@ -11,7 +10,7 @@ public interface BetDAO {
     final public static String WAIT = "WAIT";
 
     public void setStatus();
-    public Integer createGroup(Double amount, Double coefficient, Integer betsCount);
+    public Integer createGroup(Integer userID, Double amount, Double coefficient, Integer betsCount);
     public Integer createBet(Integer groupID, Integer gameID, String betType);
     public Map<Integer,Bet> getBets();
 
