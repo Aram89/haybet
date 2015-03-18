f19d2f89ff59db9420309bea0b0a611655f78a5b
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-18 22:04:54 +0400

    createGroup restored

--- haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
+++ haybet/src/main/java/org/proffart/bet/dao/BetDAOImpl.java
@@ -1,15 +1,12 @@
 package org.proffart.bet.dao;
 
-import java.security.acl.Owner;
 import java.sql.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 import org.hibernate.Criteria;
-import org.hibernate.Query;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
@@ -26,19 +23,19 @@ public class BetDAOImpl extends AbstractDAO implements BetDAO {
 
     }
 
-    public Integer createGroup(Double amount, Double coefficient, Integer betsCount) {
-        int groupID = 0;
-        BetGroup betGroup = new BetGroup();
-        betGroup.setUserID(0); //TODO user get session
-        betGroup.setAmount(amount);
-        betGroup.setCoefficient(coefficient);
-        betGroup.setBetsCount(betsCount);
-        betGroup.setFinishedBetsCount(0);
-        betGroup.setStatus(WAIT);
-        getSession().save(betGroup);
-        groupID = betGroup.getID();
-        return groupID;
-    }
+	public Integer createGroup(Integer userID, Double amount, Double coefficient, Integer betsCount) {
+		int groupID = 0;
+		BetGroup betGroup = new BetGroup();
+		betGroup.setUserID(userID);
+		betGroup.setAmount(amount);
+		betGroup.setCoefficient(coefficient);
+		betGroup.setBetsCount(betsCount);
+		betGroup.setFinishedBetsCount(0);
+		betGroup.setStatus(WAIT);
+		getSession().save(betGroup);
+		groupID = betGroup.getID();
+		return groupID;
+	}
 
     public Integer createBet(Integer groupID, Integer gameID, String betType) {
         int betID = 0;
