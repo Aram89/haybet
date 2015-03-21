7cc78cec5ce13b3be0c022640cdcb199597acf15
ashmna <mnatsakanyan.ashot@gmail.com>
2015-03-22 00:43:54 +0400

    code style

--- haybet/src/main/java/org/proffart/bet/controller/BetController.java
+++ haybet/src/main/java/org/proffart/bet/controller/BetController.java
@@ -6,13 +6,11 @@ import java.util.List;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.type.TypeFactory;
 import org.proffart.bet.domain.BetTmpStructure;
 import org.proffart.bet.domain.Events;
 import org.proffart.bet.domain.Game;
-import org.proffart.bet.domain.TmpResult;
-import org.proffart.bet.service.GameService;
 import org.proffart.bet.service.BetService;
+import org.proffart.bet.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -21,49 +19,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
-
-
 @Controller
 public class BetController {
 	@Autowired
 	GameService service;
-	
 	@Autowired
 	BetService betService;
 
-    @RequestMapping(value="bets", method = RequestMethod.GET)
-    public void getBets(ModelMap model){
-        betService.getBets();
-    }
+	@RequestMapping(value = "bets", method = RequestMethod.GET)
+	public void getBets(ModelMap model) {
+		betService.getBets();
+	}
 
-
-    @RequestMapping(value="games", method = RequestMethod.GET)
-	public String getEvents(ModelMap model){
+	@RequestMapping(value = "games", method = RequestMethod.GET)
+	public String getEvents(ModelMap model) {
 		Events event = new Events();
 		event.setTournamnetsByCounty(service.getTournamnetsByCounty());
-		model.addAttribute("event",event);
+		model.addAttribute("event", event);
 		return "games";
 	}
 
-	@RequestMapping(value="games",params = {"tournamentID"}, method = RequestMethod.GET)
-	public @ResponseBody List<Game> getGames(@RequestParam(value = "tournamentID") String tournamentID,ModelMap model){
-		List <Game> games = null;
-		try{
+	@RequestMapping(value = "games", params = { "tournamentID" }, method = RequestMethod.GET)
+	public @ResponseBody List<Game> getGames( @RequestParam(value = "tournamentID") String tournamentID, ModelMap model) {
+		List<Game> games = null;
+		try {
 			int id = Integer.parseInt(tournamentID);
-			games = service.getGames(id);			
-		}
-		catch (Exception e){
+			games = service.getGames(id);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return games;		
+		return games;
 	}
-	
-	@RequestMapping(value="games/bet",params = {"data"}, method = RequestMethod.POST)
-	public @ResponseBody BetTmpStructure doBet(@RequestParam(value = "data") String data, ModelMap model) throws
-	JsonParseException, JsonMappingException, IOException {
+
+	@RequestMapping(value = "games/bet", params = { "data" }, method = RequestMethod.POST)
+	public @ResponseBody BetTmpStructure doBet( @RequestParam(value = "data") String data, ModelMap model) throws JsonParseException, JsonMappingException, IOException {
 		BetTmpStructure betJSON = new ObjectMapper().readValue(data, BetTmpStructure.class);
 		betService.doBet(betJSON);
 		return betJSON;
-	}	
+	}
 }
